4bd3e6b3304e1ab6f33083f3541bced9caf076a7
Julles Pio <juliokirk@gmail.com>
2024-10-03 19:11:37 -0300

    Small adjustments

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = 'Captain.';
+const user = 'Captain.';
 
 // Here you can change your greetings
 const gree1 = 'Go to sleep,  ';
@@ -15,11 +15,11 @@ const gree6 = 'Good evening,  ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
+  document.getElementById('greetings').innerText = gree1 + user;
 } else if (hour >= 6 && hour < 12) {
-  document.getElementById('greetings').innerText = gree2 + name;
+  document.getElementById('greetings').innerText = gree2 + user;
 } else if (hour >= 12 && hour < 17) {
-  document.getElementById('greetings').innerText = gree3 + name;
+  document.getElementById('greetings').innerText = gree3 + user;
 } else {
-  document.getElementById('greetings').innerText = gree4 + name;
+  document.getElementById('greetings').innerText = gree4 + user;
 }
