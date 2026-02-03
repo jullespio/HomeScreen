3cbe5b3c5e00997bc07a31921d4b36873bd3f176
Julles Pio <jullespio@tutanota.com>
2026-02-03 19:06:12 -0300

    Small update to Stardate calculations

--- js/stardate.js
+++ js/stardate.js
@@ -6,7 +6,7 @@ function calculateStardate() {
     // const then = new Date("July 15, 1987"); // Original TNG reference date
     const then = new Date("2000-01-01T00:00:00Z"); // Revised reference date
     
-    let stardate = (now.getTime() - then.getTime()) / (1000 * 60 * 60 * 24 * 0.036525);
+    let stardate = (now.getTime() - then.getTime()) / (1000 * 60 * 60 * 24 * 0.03652425);
     // stardate = Math.floor(stardate + 410000) / 10; // Original formula (TNG era)
     stardate = Math.floor(stardate) / 10;
 
